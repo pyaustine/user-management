250a710161fbf6b1aa2d3391e72ff5757a73b858
Ayah-Austin <austinjuniour4@gmail.com>
2023-01-17 11:01:18 +0300

    initial commit

---
+++ users/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+
+class UserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'users'
+
+
+
+
+    def ready(self):
+        import users.signals #quna
